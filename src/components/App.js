d8d096baf1df603160f4c5065814302001f31f23
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-06-16 10:56:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,51 @@ const App = () => {
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
+  let secondElapsed = 0;
+  const startbtn = () => {
+    startTime.current = Date.now()
+    intervalRef.current = setInterval(()=>{
+      setCurrentTime(Date.now())
+    },10)
+  }
+    const stopbtn = ()=>{
+    clearInterval(intervalRef.current)
+      intervalRef.current =0
+    }
+  secondElapsed = ((currentTime - startTime.current)/1000)
+  const resetbtn = () => {
+    setCurrentTime(0);
+    startTime.current = 0;
+    secondElapsed =0;
+    setLaps([])
+  }
 
+  const trackLapes = () => {
+    setLaps([...laps,secondElapsed])
+  }
+  
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{secondElapsed.toFixed(3)} Time</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button onClick={startbtn} className="start-btn">START</button>
+          <button onClick={stopbtn} className="stop-btn">STOP</button>
+          <button onClick={trackLapes} className="lap-btn">LAP</button>
+          <button onClick={resetbtn} className="reset-btn">RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
+{laps.length ? (
+  <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+  {laps.map((e,i) => (
+    <p key={i}>{e}</p>
+  ))}
         </section>
       </section>
+):null}
+      
     </div>
   )
 }
